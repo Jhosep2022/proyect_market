a49bbe69fa4e6cdfdfddc97bceede1a40421f6d3
Jhosep2022 <jose.condori.r@ucb.edu.bo>
2023-07-03 18:34:43 -0400

    agregacion de los mapeos

---
+++ src/main/java/com/platzi/proyectmarket/persistence/mapper/CategoryMapper.java
@@ -0,0 +1,21 @@
+package com.platzi.proyectmarket.persistence.mapper;
+
+import com.platzi.proyectmarket.domain.Category;
+import com.platzi.proyectmarket.persistence.entity.Categoria;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper (componentModel = "spring")
+public interface CategoryMapper {
+    @Mappings({
+        @Mapping(source = "idCategoria", target = "categoryId"),
+        @Mapping(source = "descripcion", target = "category"),
+        @Mapping(source = "estado", target = "active"),
+    })
+    Category toCatergory(Categoria categoria);
+    @InheritInverseConfiguration
+    @Mapping(target = "productos", ignore = true)
+    Categoria toCategoria(Category category);
+}
